a2f263592247caa6e310bfadff239a3deb1f33aa
gunhee93 <123151812+gunhee93@users.noreply.github.com>
2023-08-02 21:17:32 +0900

    setup

---
+++ src/test/java/wantedpreonboarding/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package wantedpreonboarding.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
